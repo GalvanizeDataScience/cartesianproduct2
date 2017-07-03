a9131ea64ca11406c7f0d44927f12cab2632bde9
Jaime Almeida <jaime.almeida@gatech.edu>
2017-07-03 10:02:24 -0700

    Added code to parse web API response

--- build.sbt
+++ build.sbt
@@ -10,8 +10,9 @@ libraryDependencies += "org.apache.kafka" %% "kafka" % "0.10.1.1"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-streaming" % sparkVersion,
-  "org.apache.spark" % "spark-streaming-kafka-0-8_2.11" % sparkVersion
+  "org.apache.spark" % "spark-streaming-kafka-0-8_2.11" % sparkVersion,
 //"org.apache.spark" %% "spark-streaming-kafka" % sparkVersion
+  "org.json4s" %% "json4s-native" % "3.2.11"
 )
 
 
--- src/main/scala/KafkaBroker.scala
+++ src/main/scala/KafkaBroker.scala
@@ -1,5 +1,13 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.Seconds
 import java.util.{Date, Properties}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.spark.streaming.kafka.KafkaUtils
+import scala.io._
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
 
 import scala.util.Random
 
@@ -10,18 +18,27 @@ import scala.util.Random
   */
 
 object KafkaBroker {
+
+  val conf = new SparkConf().setMaster("local[*]").setAppName("KafkaReceiver")
+
+  // Spark Streaming is now connected to Apache Kafka and consumes messages every 10 seconds
+  val ssc = new StreamingContext(conf, Seconds(10))
+
+
   val events = args(0).toInt
   val topic = args(1)
-  val brokers = args(2)
+  val brokers = args(2) // "broker1:port,broker2:port"
   val rnd = new Random()
+
   val props = new Properties()
   props.put("bootstrap.servers", brokers)
-  props.put("client.id", "ScalaProducerExample")
+  props.put("client.id", "ScalaProducer") // 0, 1, 2 in tutorial
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-
   val producer = new KafkaProducer[String, String](props)
+
+
   val t = System.currentTimeMillis()
   for (nEvents <- Range(0, events)) {
     val runtime = new Date().getTime()
@@ -33,6 +50,39 @@ object KafkaBroker {
     //producer.send(data, (m,e) => {})
     //sync
     producer.send(data)
+
   }
+
+  val producerRecord = new ProducerRecord[String, String](topicName, data)
+  val recordMetadata = producer.send(producerRecord);
+
+
+  // need to take token out of request
+
+
+  // query web API - response will be a String
+  val response = Source.fromURL(
+    "https://api.plume.io/1.0/pollution/forecast?token=xxx&lat=48.85&lon=2.294"
+  ).mkString
+
+  // process String as JSON
+  val jsonResponse = parse(response)
+
+  // Go into the JObject instance and bind the list of fields to the constant fields
+  val JObject(fields) = jsonResponse
+
+  val firstField = fields.head
+  val JField(key, JString(value)) = firstField
+
+  // Extracting fields using XPath
+  val JString(loginName) = jsonResponse \ "login"
+
+
+  // name of
+  val kafkaStream = KafkaUtils.createStream(ssc, "localhost:2181","spark-streaming-consumer-group", Map("plume_pollution" -> 5))
+
+  kafkaStream.print()
+
+  ssc.start
 
 }
