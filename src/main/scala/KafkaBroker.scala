94bc51f37bff84b3e2cbdb98210232fe6cd65a06
Jaime Almeida <jaime.almeida@gatech.edu>
2017-07-03 13:49:35 -0700

    Broker working - removed commented code

--- src/main/scala/KafkaBroker.scala
+++ src/main/scala/KafkaBroker.scala
@@ -1,21 +1,6 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.Seconds
-import java.util.{Date, Properties}
-
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
-import org.apache.spark.streaming.kafka.KafkaUtils
-
+import java.util.Properties
 import scala.io._
-import org.json4s._
-import org.json4s.native.JsonMethods._
-
-import scala.concurrent.Future
-//import org.json4s.jackson.JsonMethods._
-// https://forums.databricks.com/questions/6928/cannot-parse-json-using-json4s-on-databricks.html
-
-
-import scala.util.Random
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 /**
   * Kafka broker to ingest data from plume.io
@@ -27,7 +12,7 @@ object KafkaBroker extends App {
 
   override def main(args: Array[String]): Unit = {
 
-    // access plume token
+    // access plume token https://github.com/zipfian/cartesianproduct2/wiki/TOKEN
     lazy val token:Option[String] = sys.env.get("PLUMETOKEN") orElse {
       println("No token found. Check how to set it up at https://github.com/zipfian/cartesianproduct2/wiki/TOKEN")
       None
@@ -52,10 +37,9 @@ object KafkaBroker extends App {
 //    props.put("linger.ms", new Integer(1))
 //    props.put("buffer.memory", new Integer(133554432))
 
-    // query API on an infinity loop with 'sleepTime' intervals
-
     // TODO: implement ProducerCallback()
 
+    // query API on an infinity loop with 'sleepTime' intervals
     while (true){
 
       // create producer with 'props' properties
@@ -66,31 +50,15 @@ object KafkaBroker extends App {
         "https://api.plume.io/1.0/pollution/forecast?token="+ token.get +"&lat="+ lat +"&lon="+ lon
       ).mkString
 
-
-      // decided to keep the response as text but below is the code to further process the String as JSON
-//      val jsonResponse = parse(response)
-
-      // Go into the JObject instance and bind the list of fields to the constant fields
-//      val JObject(fields) = jsonResponse
-
-      //    val firstField = fields.head
-      //    val JField(key, JDouble(value)) = firstField
-      //      key: String = latitude
-      //      value: Double = 48.85
-
-      // Extracting fields using XPath
-      //    val JDouble(longitude) = jsonResponse \ "longitude"
-      //      longitude: Double = 2.294
-
       val producerRecord = new ProducerRecord[String, String](topic, response)
       val recordMetadata = producer.send(producerRecord)
 
-      val meta = recordMetadata.get() // I could use this to write tests
+      val meta = recordMetadata.get() // I could use this to write some tests
       val msgLog =
         s"""
+           |topic     = ${meta.topic()}
            |offset    = ${meta.offset()}
            |partition = ${meta.partition()}
-           |topic     = ${meta.topic()}
           """.stripMargin
       println(msgLog)
 
