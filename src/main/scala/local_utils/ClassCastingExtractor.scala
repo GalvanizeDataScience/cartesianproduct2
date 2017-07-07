81582ad54c68c72a2756bfd561808380d330b859
Daniel Gorham <daniel@vidora.com>
2017-07-07 10:00:55 -0700

    Utilities to parse nested JSON using an extractor class that casts objects to some other datatype.

--- src/main/scala/local_utils/ClassCastingExtractor.scala
+++ src/main/scala/local_utils/ClassCastingExtractor.scala
@@ -1,8 +1,16 @@
-package utils
+package local_utils
 
 /**
   * Created by daniel on 7/6/17.
   */
+
+/*
+Utilities to cast objects to various datatypes. Useful for parsing JSON from strings using only Scala's
+built in JSON parser, which only returns a String -> String map.
+
+See here: https://stackoverflow.com/questions/4170949/how-to-parse-json-in-scala-using-standard-scala-classes
+ */
+
 class ClassCastingExtractor[T] {
   def unapply(a: Any): Option[T] = {
     Some(a.asInstanceOf[T])
